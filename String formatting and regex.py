102aafca779e8f306386c3e09f439c4349f2cf99
Aleksander <kaluskialeksander@gmail.com>
2025-12-08 17:31:32 +0100

    + up additional regex patterns

--- String formatting and regex.py
+++ String formatting and regex.py
@@ -93,17 +93,34 @@ pattern.findall(text)
 # -----------------------------------------------------------
 # Regular Expression Quantifiers:
 #
-# Quantifier | Meaning                   | Example
-# -------------------------------------------------------------------
-# *          | 0 or more occurrences     | a*b matches b, ab, aab
-# +          | 1 or more occurrences     | a+b matches ab, aab (but not b)
-# ?          | 0 or 1 occurrence         | a?b matches b or ab
-# {n}        | Exactly n occurrences     | a{3}b matches aaab
-# {n,m}      | Between n & m occurrences | a{2,4}b matches aab, aaab, aaaab
-# {n,}       | Minimum n occurrences     | a{2,}b matches aab, aaab, and so on
-# {,m}       | Up to m occurrences       | a{,3}b matches b, ab, aab, aaab
+# REGULAR EXPRESSION REFERENCE: Quantifiers, Classes, and Sets
 #
-# Note: Append '?' to make a quantifier lazy (e.g., *? or +?).
+# Pattern    | Type       | Meaning                                    | Example
+# ------------------------------------------------------------------------------------------------
+# **Quantifiers**
+# *          | Quantifier | 0 or more occurrences                      | r'a*b' matches 'b', 'ab', 'aab'
+# +          | Quantifier | 1 or more occurrences                      | r'a+b' matches 'ab', 'aab' (but not 'b')
+# ?          | Quantifier | 0 or 1 occurrence (optional)               | r'a?b' matches 'b' or 'ab'
+# {n}        | Quantifier | Exactly n occurrences                      | r'a{3}b' matches 'aaab'
+# {n,m}      | Quantifier | Between n & m occurrences (inclusive)      | r'a{2,4}b' matches 'aab' to 'aaaab'
+# {n,}       | Quantifier | Minimum n occurrences                      | r'a{2,}b' matches 'aab', 'aaab', and so on
+# {,m}       | Quantifier | Up to m occurrences                        | r'a{,3}b' matches 'b' up to 'aaab'
+#
+# **Predefined Character Classes**
+# \d         | Class      | Matches any single digit (0-9)                      | r'\d+' matches '100', '5'
+# \D         | Class      | Matches any single non-digit                        | r'\D' matches 'a', '!'
+# \w         | Class      | Matches any single alphanumeric (A-z, 0-9, _)       | r'\w+' matches 'word', 'id_123'
+# \W         | Class      | Matches any single non-alphanumeric character       | r'\W' matches '$', '#'
+# \s         | Class      | Matches any single whitespace (space, tab, newline) | r'\s+' matches a sequence of spaces
+# \S         | Class      | Matches any single non-whitespace character         | r'\S+' matches 'word'
+#
+# **Character Sets (Brackets)**
+# [abc]      | Set        | Matches any single character: 'a', 'b', or 'c'            | r'[aeiou]' matches a single vowel
+# [a-z]      | Set Range  | Matches any single character in the range 'a' through 'z' | r'[A-Z]{3}' matches 'ABC'
+# [0-9a-f]   | Set Range  | Matches any single digit or hex letter 'a' through 'f'    | r'[0-9a-f]+' matches a hex string
+# [^abc]     | Negated Set| Matches any single character NOT in the set               | r'[^0-9]+' matches a sequence of non-digits
+#
+# Note: Append '?' to a quantifier to make it lazy (e.g., *? or +?).
 
 # -----------------------------------------------------------
 # Greedy and Lazy Maching
