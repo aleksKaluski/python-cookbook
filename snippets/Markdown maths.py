7279f3b1e92812aa8f12c908343897465359efd8
Aleksander Kałuski <kaluskialeksander@gmail.com>
2026-04-14 15:20:29 +0200

    +Markdown math

---
+++ snippets/Markdown maths.py
@@ -0,0 +1,36 @@
+"""
+LATEX / MARKDOWN MATH CHEAT SHEET
+Wrapper: Use $...$ for inline or $$...$$ for blocks.
+
+-- BASICS --
+Superscript:    x^{2}           -> x²
+Subscript:      x_{i}           -> xi
+Square Root:    \sqrt{x}        -> √x
+N-th Root:      \sqrt[n]{x}     -> ⁿ√x
+Fraction:       \frac{a}{b}     -> a/b
+
+-- CALCULUS --
+Derivative:     \frac{dy}{dx}
+Partial Diff:   \frac{\partial f}{\partial x}
+Integral:       \int_{a}^{b}
+Summation:      \sum_{i=1}^{n}
+Limit:          \lim_{x \to \infty}
+
+-- ACCENTS --
+Hat/Estimator:  \hat{x}
+Mean/Bar:       \bar{x}
+Vector:         \vec{v}
+Dot/Deriv:      \dot{a}
+Tilde:          \tilde{n}
+
+-- GREEK --
+Alpha/Beta:     \alpha, \beta
+Delta:          \delta, \Delta
+Pi/Sigma:       \pi, \Sigma
+Theta/Omega:    \theta, \Omega
+
+-- FORMATTING --
+Auto-size Brackets: \left( \frac{a}{b} \right)
+Text in Math:       \text{word}
+Spaces:             \, (thin), \; (thick)
+"""
